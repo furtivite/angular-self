37c25217d9fdecc2e2382e50837b6052103f7443
Egor Levchenko <antytoto@gmail.com>
2024-05-23 20:51:58 +0300

    moves home template

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,25 +8,7 @@ import { HousingLocation } from '../../interfaces';
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule, HousingLocationComponent],
-  template: `
-    <section>
-      <form>
-        <input type="text" placeholder="Filter by city" #filter>
-        <button
-          class="primary"
-          type="button" (click)="filterResults(filter.value)"
-        >
-          Search
-        </button>
-      </form>
-    </section>
-    <section class="results">
-      <app-housing-location
-        *ngFor="let housingLocation of filteredLocationList"
-        [housingLocation]="housingLocation"
-      ></app-housing-location>
-    </section>
-  `,
+  templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
